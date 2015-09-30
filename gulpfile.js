9fb127f8701e61166bb4f9630fec07bfc9d04c09
Marius Kintel <marius@kintel.net>
2015-09-30 12:24:14 -0400

    watch models

--- gulpfile.js
+++ gulpfile.js
@@ -42,5 +42,6 @@ gulp.task('serve', ['bower', 'browserify', 'copy'], function () {
 gulp.task('watch', ['serve'], function () {
     gulp.watch(['public/*.html'], reload);
     gulp.watch(['src/**/*.js'], ['browserify'], reload);
+    gulp.watch(['models/**/*'], ['copy'], reload);
     gulp.watch(['bower.json'], ['copy'], reload);
 });
